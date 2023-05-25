715d637abb391d03d152f1b466af819ef0b7ef61
ffasdd <nhc0830@naver.com>
2023-05-26 01:57:43 +0900

    time, gif 추가
    시간과 gif를 추가 하였다

--- pythonProject34/main.py
+++ pythonProject34/main.py
@@ -1,18 +1,65 @@
-import tkinter as tk
-import tkinter.ttk as ttk
-import requests
-import xml.etree.ElementTree as ET
-from PIL import Image,ImageTk
-import io
-from googlemaps import Client
+from tkinter import *
+from tkinter import font
+import datetime
 
-zoom =13
-api_key = "sea100UMmw23Xycs33F1EQnumONR/9ElxBLzkilU9Yr1oT4TrCot8Y2p0jyuJP72x9rG9D8CN5yuEs6AS2sAiw=="
-url = "http://apis.data.go.kr/B551182/hospInfoServicev2/getHospBasisList"
-params={
-    "serviceKey":api_key
-    "numOfRows":350
-    "sidoCd":110000
-}
-response=requests.get(url,params=params)
-root=ET.fromstring
+g_Tk = Tk()
+g_Tk.geometry("1000x600")
+DataList = []
+url = "openapi.nature.go.kr"
+query = "/openapi/service/rest/FungiService/fngsSpcmInfo?ServiceKey=fGahpMpOdPXZYI3PiwdkIW%2BXFL6ElAoipUQonJDz7xVIbvq7ZipdgE1jIdrHjVztgXaFZA2AUpuKAqSyS9GtCg%3D%3D&q1=FB2012112400000141"
+
+# 버섯표본 번호를 적어줘야해용
+Q1 = "FB2012112400000141"
+
+# gif 관련 변수에용
+frameCnt = 4
+frames = [PhotoImage(file='MR.gif', format='gif -index %i' % i).subsample(2) for i in range(frameCnt)]
+
+# 시간 관련
+date = datetime.date.today()
+
+def InitTopText():
+    TempFont = font.Font(g_Tk, size=20, weight='bold', family='Consolas')
+    MainText = Label(g_Tk, font=TempFont, text="[버섯도리]")
+    MainText.pack()
+    MainText.place(x=400)
+
+def InitGif(ind):
+    frame = frames[ind]
+    ind += 1
+    if ind == frameCnt:
+        ind = 0
+    label.configure(image=frame)
+    g_Tk.after(100, InitGif, ind)
+
+def update_clock():
+    now = datetime.datetime.now()
+    if(int(now.strftime("%H")) >= 13):
+        hour = int(now.strftime("%H")) - 12
+        current_time = now.strftime("오후 "+hour+":%M")
+    else:
+        current_time = now.strftime("오전 %H:%M")
+    current_date = now.strftime("%Y년 %m월 %d일")
+    time_label.config(text=current_time)
+    date_label.config(text=current_date)
+    g_Tk.after(1000, update_clock)
+    date_label.place(x=750,y=500)
+    time_label.place(x=800,y=550)
+
+InitTopText()
+
+# gif 부분 입니다
+label = Label(g_Tk, bg='white')
+label.place(x=50, y=50, anchor=CENTER)
+g_Tk.after(0, InitGif, 0)
+
+# 시간 관련 부분입니다
+date_label = Label(g_Tk, font=("Arial", 20))
+date_label.pack()
+
+time_label = Label(g_Tk, font=("Arial", 18))
+time_label.pack()
+
+update_clock()
+
+g_Tk.mainloop()
